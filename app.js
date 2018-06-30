e61a08fe71e77a32ec3ddfb85a1e01592804272f
kburt <kellie.burt@gmail.com>
2018-06-29 22:20:27 -0500

    create pages

---
+++ app.js
@@ -0,0 +1,28 @@
+$(function() {
+    $(form).on("submit", function(e){
+        e.preventDefault();
+        //prepare the request
+        var request = gapi.client.youtube.search.list({
+            part: "snippet", 
+            type: "video", 
+            q: encodeURIComponent($(#search).val()).replace(/%20/, "+"),
+            maxResults: 10,
+            order: "viewCount", 
+            publishedAfter: "2015-01-01T00:00:00Z"
+        });
+        //execute the request
+        request.execute(function(response) {
+            console.log(response);
+        });
+    });
+});
+
+
+
+
+function init(){
+    gapi.client.setApiKey("AIzaSyBxfjuYY3-e5tBLybo39NRs3mlkkHZYnJQ");
+    gapi.client.load("youtube", "v3", function() {
+        //youtube api key is ready
+    });
+}
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+    try {
+        $conn = new PDO("mysql:host=$servername", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE DATABASE myDBPDO";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        echo "Database created successfully<br>";
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+    
+    $conn = null;
+?>
